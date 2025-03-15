c455f33975966f1c4754fc1ce5e3d6dea5c5899c
Leonardo <leondani853@gmail.com>
2025-03-15 01:51:40 -0300

    feat: add custom method to count the days

--- src/main/java/com/timeclock/api/repository/TimeEntryRepository.java
+++ src/main/java/com/timeclock/api/repository/TimeEntryRepository.java
@@ -4,6 +4,9 @@ import com.timeclock.api.domain.TimeEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface TimeEntryRepository extends JpaRepository<TimeEntry, Long> {
+    Long countByTimestampBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
