4a55c224395a38ef9c497a3cc7c039cefb5d30d9
Leonardo <leondani853@gmail.com>
2025-03-15 16:02:31 -0300

    feat: add method calculate total worked hours

--- src/main/java/com/timeclock/api/service/TimeEntryService.java
+++ src/main/java/com/timeclock/api/service/TimeEntryService.java
@@ -4,10 +4,15 @@ import com.timeclock.api.domain.TimeEntry;
 import com.timeclock.api.exception.EntriesPerDayException;
 import com.timeclock.api.exception.WeekendEntryNotAllowedException;
 import com.timeclock.api.repository.TimeEntryRepository;
+import com.timeclock.api.response.TotalWorkedHoursResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,6 +41,37 @@ public class TimeEntryService {
         if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
             throw new WeekendEntryNotAllowedException("You cannot register on weekends");
         }
+    }
+
+    public double calculateWorkedHours(LocalDate date) {
+        var startDate = date.atStartOfDay();
+        var endDate = startDate.plusDays(1);
+
+        List<TimeEntry> entries = repository.findByTimestampBetween(startDate, endDate);
+
+        if (entries.isEmpty()) {
+            return 0.0;
+        }
+
+        if (entries.size() < 4) {
+            throw new IllegalArgumentException("At least 4 entries are required to calculate worked hours.");
+        }
+
+        if (entries.size() % 2 != 0) {
+            entries = entries.subList(0, entries.size() - 1);
+        }
+
+        double totalMinutes = 0.0;
+
+        for (int i = 0; i < entries.size(); i += 2) {
+            var clockIn = entries.get(i).getTimestamp();
+            var clockOut = entries.get(i + 1).getTimestamp();
+            totalMinutes += Duration.between(clockIn, clockOut).toMinutes();
+        }
+
+        return totalMinutes / 60.0;
+
+
     }
 
 }
