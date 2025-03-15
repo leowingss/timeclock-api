2a730ee229f50e9eba5881d92f43397d30f52eee
Leonardo <leondani853@gmail.com>
2025-03-15 04:40:07 -0300

    refactor: add timestamp attribute to TimeEntryPostRequest

--- src/main/java/com/timeclock/api/request/TimeEntryPostRequest.java
+++ src/main/java/com/timeclock/api/request/TimeEntryPostRequest.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
 public class TimeEntryPostRequest {
     private EntryType type;
-
+    private LocalDateTime timestamp;
 }
--- src/main/java/com/timeclock/api/response/TimeEntryPostResponse.java
+++ src/main/java/com/timeclock/api/response/TimeEntryPostResponse.java
@@ -1,15 +1,16 @@
 package com.timeclock.api.response;
 
 import com.timeclock.api.domain.EntryType;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TimeEntryPostResponse {
     private Long id;
     private String type;
