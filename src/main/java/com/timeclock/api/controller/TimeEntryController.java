2a0c920291968d6b3229560978ba6fcb8d678865
Leonardo <leondani853@gmail.com>
2025-03-15 16:18:52 -0300

    docs: add OpenAPI for worked hours

--- src/main/java/com/timeclock/api/controller/TimeEntryController.java
+++ src/main/java/com/timeclock/api/controller/TimeEntryController.java
@@ -1,12 +1,17 @@
 package com.timeclock.api.controller;
 
 import com.timeclock.api.domain.TimeEntry;
+import com.timeclock.api.exception.DefaultErrorMessage;
 import com.timeclock.api.mapper.TimeEntryMapper;
 import com.timeclock.api.request.TimeEntryPostRequest;
 import com.timeclock.api.response.TimeEntryPostResponse;
 import com.timeclock.api.response.TotalWorkedHoursResponse;
 import com.timeclock.api.service.TimeEntryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -45,6 +50,17 @@ public class TimeEntryController {
     }
 
     @GetMapping("/worked-hours")
+    @Operation(
+            summary = "Get total worked hours for a specific day",
+            description = "Retrieve the total number of hours worked. If no records exist, it returns 0.0"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Total worked hours retrieved successfully",
+                    content = @Content(schema = @Schema(implementation = TotalWorkedHoursResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid date format or insufficient entries",
+                    content = @Content(schema = @Schema(implementation = DefaultErrorMessage.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<TotalWorkedHoursResponse> getWorkedHours(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
 
         double totalHours = service.calculateWorkedHours(date);
