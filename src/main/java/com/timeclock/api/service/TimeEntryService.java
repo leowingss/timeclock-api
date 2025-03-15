43636550b12931382ae64d963c64cb6dfe6f82c5
Leonardo <leondani853@gmail.com>
2025-03-15 01:15:33 -0300

    feat: add weekend validation

--- src/main/java/com/timeclock/api/service/TimeEntryService.java
+++ src/main/java/com/timeclock/api/service/TimeEntryService.java
@@ -1,9 +1,12 @@
 package com.timeclock.api.service;
 
 import com.timeclock.api.domain.TimeEntry;
+import com.timeclock.api.exception.WeekendEntryNotAllowedException;
 import com.timeclock.api.repository.TimeEntryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +14,15 @@ public class TimeEntryService {
     private final TimeEntryRepository repository;
 
     public TimeEntry registerTimeEntry(TimeEntry timeEntry) {
+        checkIfIsWeekend(timeEntry);
         return repository.save(timeEntry);
+    }
+
+    private static void checkIfIsWeekend(TimeEntry timeEntry) {
+        var dayOfWeek = timeEntry.getTimestamp().getDayOfWeek();
+        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+            throw new WeekendEntryNotAllowedException("You cannot register on weekends");
+        }
     }
 
 }
