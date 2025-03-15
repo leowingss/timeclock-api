cf4d1dcda3725579907e3237c4a4a95ddce4d01e
Leonardo <leondani853@gmail.com>
2025-03-14 23:30:43 -0300

    feat: create TimeEntry service

---
+++ src/main/java/com/timeclock/api/service/TimeEntryService.java
@@ -0,0 +1,19 @@
+package com.timeclock.api.service;
+
+import com.timeclock.api.domain.TimeEntry;
+import com.timeclock.api.repository.TimeEntryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TimeEntryService {
+
+    private TimeEntryRepository repository;
+
+    public TimeEntry registerTimeEntry(TimeEntry timeEntry) {
+        return repository.save(timeEntry);
+    }
+
+
+}
