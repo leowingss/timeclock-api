7cfe9afba83fec0a1051d38ab8ec3b07b448bbba
Leonardo <leondani853@gmail.com>
2025-03-15 00:45:36 -0300

    refactor: switch dependency injection to lombok annotations

--- src/main/java/com/timeclock/api/service/TimeEntryService.java
+++ src/main/java/com/timeclock/api/service/TimeEntryService.java
@@ -2,15 +2,13 @@ package com.timeclock.api.service;
 
 import com.timeclock.api.domain.TimeEntry;
 import com.timeclock.api.repository.TimeEntryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TimeEntryService {
     private final TimeEntryRepository repository;
-
-    public TimeEntryService(TimeEntryRepository repository ) {
-        this.repository = repository;
-    }
 
     public TimeEntry registerTimeEntry(TimeEntry timeEntry) {
         return repository.save(timeEntry);
