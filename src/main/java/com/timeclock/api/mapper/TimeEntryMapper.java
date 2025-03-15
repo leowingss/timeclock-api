e8f2d90d7a00be6685db7daae0138392be286ab3
Leonardo <leondani853@gmail.com>
2025-03-15 16:11:01 -0300

    feat: create default mapper to total worked hours response

--- src/main/java/com/timeclock/api/mapper/TimeEntryMapper.java
+++ src/main/java/com/timeclock/api/mapper/TimeEntryMapper.java
@@ -3,9 +3,12 @@ package com.timeclock.api.mapper;
 import com.timeclock.api.domain.TimeEntry;
 import com.timeclock.api.request.TimeEntryPostRequest;
 import com.timeclock.api.response.TimeEntryPostResponse;
+import com.timeclock.api.response.TotalWorkedHoursResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+
+import java.time.LocalDate;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TimeEntryMapper {
@@ -14,5 +17,9 @@ public interface TimeEntryMapper {
     TimeEntry toTimeEntryEntity(TimeEntryPostRequest timeEntryPostRequest);
 
     TimeEntryPostResponse toTimeEntryPostResponse(TimeEntry timeEntry);
+
+    default TotalWorkedHoursResponse toTotalWorkedHoursResponse(LocalDate date, double totalHours) {
+        return new TotalWorkedHoursResponse(date, totalHours);
+    }
 
 }
