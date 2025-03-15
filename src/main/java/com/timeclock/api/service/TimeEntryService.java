29edb9434005699a879103e6cab37c197992c092
Leonardo <leondani853@gmail.com>
2025-03-15 01:52:26 -0300

    feat: add check entries per day method

--- src/main/java/com/timeclock/api/service/TimeEntryService.java
+++ src/main/java/com/timeclock/api/service/TimeEntryService.java
@@ -1,6 +1,7 @@
 package com.timeclock.api.service;
 
 import com.timeclock.api.domain.TimeEntry;
+import com.timeclock.api.exception.EntriesPerDayException;
 import com.timeclock.api.exception.WeekendEntryNotAllowedException;
 import com.timeclock.api.repository.TimeEntryRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,19 @@ public class TimeEntryService {
 
     public TimeEntry registerTimeEntry(TimeEntry timeEntry) {
         checkIfIsWeekend(timeEntry);
+        checkEntriesPerDay(timeEntry);
         return repository.save(timeEntry);
+    }
+
+    private void checkEntriesPerDay(TimeEntry timeEntry) {
+        var startDate = timeEntry.getTimestamp().toLocalDate().atStartOfDay();
+        var endDate = startDate.plusDays(1);
+
+        long countDays = repository.countByTimestampBetween(startDate, endDate);
+
+        if (countDays >= 4) {
+            throw new EntriesPerDayException("Only 4 entries per day");
+        }
     }
 
     private static void checkIfIsWeekend(TimeEntry timeEntry) {
