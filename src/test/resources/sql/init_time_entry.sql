dbf148774b0a0e943f0ae838a0eb06164262de9d
Leonardo <leondani853@gmail.com>
2025-03-16 02:44:27 -0300

    test: create script sql for worked hours test

---
+++ src/test/resources/sql/init_time_entry.sql
@@ -0,0 +1,4 @@
+INSERT INTO time_entry (timestamp, type) VALUES ('2025-03-20T08:00:00', 'CLOCK_IN');
+INSERT INTO time_entry (timestamp, type) VALUES ('2025-03-20T12:00:00', 'CLOCK_OUT');
+INSERT INTO time_entry (timestamp, type) VALUES ('2025-03-20T13:00:00', 'CLOCK_IN');
+INSERT INTO time_entry (timestamp, type) VALUES ('2025-03-20T17:00:00', 'CLOCK_OUT');
